fc32ef7acc60b0ad0274496b2f2337d570c4e012
Anders Roxell <anders.roxell@linaro.org>
2019-03-07 15:40:38 +0100

    WIP
    Change-Id: I44185f3ee1fa9275451d577b394bfda264a79fa1
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

---
+++ automated/linux/chroot/download_and_mount.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ARM32_ROOTFS=${1}
+
+cd /
+curl -SOL "${ARM32_ROOTFS}"
+unxz rpb-console-image-lkft-*.rootfs.tar.xz
+mkdir -p /new_root
+tar --strip-components=1 -C new_root -xf rpb-console-image-lkft-*.rootfs.tar
+rm rpb-console-image-lkft-*.rootfs.tar
+cd new_root
+mount -t proc /proc proc/
+mount --rbind /sys sys/
+mount --rbind /dev dev/
+mount --rbind /run run/
+cd -
