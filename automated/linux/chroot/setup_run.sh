1d9d5214d1626596ee71bb50463fd053f4e272cc
Anders Roxell <anders.roxell@linaro.org>
2019-03-12 09:37:20 +0100

    crap
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/chroot/setup_run.sh
+++ automated/linux/chroot/setup_run.sh
@@ -10,6 +10,7 @@ echo "cat /etc/os-release" > new_root/run.sh
 echo "cd /testdef/automated/linux/${TEST_SUITE}/" >> new_root/run.sh
 
 awk "/params/{flag=1; next} /run/{flag=0} flag" new_root/testdef/automated/linux/${TEST_SUITE}/${TEST_SUITE}.yaml | sed 's/^ *//; s/: */=/' >> new_root/run.sh
+. new_root/testdef/automated/linux/chroot/${TEST_PARAM_FILE} >> new_root/run.sh
 cat new_root/testdef/automated/linux/chroot/${TEST_PARAM_FILE} >> new_root/run.sh
 awk "/cd \.\/automated\/linux/{flag=1; next} /send-to-lava\.sh/{flag=0} flag" new_root/testdef/automated/linux/${TEST_SUITE}/${TEST_SUITE}.yaml | sed 's/^ *- *//' >> new_root/run.sh
 
