96630300c17529da45bb6e8f1f87f74135c0243c
Mirco Romagnoli <romagnoli.mirco@gmail.com>
2021-04-22 14:26:33 +0200

    automated: linux: add mmtests benchmarks
    MMTests is a configurable test suite that runs performance tests
    against arbitrary workloads.
    
    This commit adds to the test set the mmtests suite and exports
    the test results in a JSON file.
    
    Signed-off-by: Mirco Romagnoli <romagnoli.mirco@gmail.com>

---
+++ automated/linux/mmtests/json-to-lava.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import argparse
+import json
+
+def main(args):
+	exitcode = 0
+	with open(args.json_file) as f:
+		module = json.load(f)
+		module_name = module["_ModuleName"].replace("Extract", "").lower()
+		test_name = module["_TestName"]
+		data = module["_ResultData"]
+		operations = list(data)
+		iterations = len(data[operations[0]])
+		for i in range(iterations):
+			for op in operations:
+				op_data = data[op][i]
+				values = op_data["Values"]
+				samples = op_data["SampleNrs"]
+				for val, sample in zip(values, samples):
+					print(f"{module_name}-{test_name}-{op}-{i}-{sample} pass {val}")
+	exit(exitcode)
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Formats mmtests loadable json to lava results format")
+	parser.add_argument('json_file', type=str, nargs='?',
+		help='JSON file to convert in a test result acceptable for lava')
+	args = parser.parse_args()
+	main(args)
