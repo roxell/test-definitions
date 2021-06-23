a67800ce3a02bd15f1948d14d45c575341a52e30
Naresh Kamboju <naresh.kamboju@linaro.org>
2021-06-23 20:23:44 +0530

    gpiod: get test sources and install and run
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>
    Signed-off-by: Naresh Kamboju <naresh.kamboju@linaro.org>

--- automated/linux/gpiod/gpiod.sh
+++ automated/linux/gpiod/gpiod.sh
@@ -1,8 +1,125 @@
 #!/bin/bash
 
-GPIOD_PATH=${1:-"/opt/libgpiod/bin/"}
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
 
-export PATH="${GPIOD_PATH}:$PATH"
+TEST_PROGRAM=gpiod
+TEST_PROG_VERSION=
+TEST_GIT_URL=https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git
+TEST_DIR="$(pwd)/${TEST_PROGRAM}"
+SKIP_INSTALL="false"
+GPIOD_PATH="/opt/libgpiod"
+
+usage() {
+	echo "\
+	Usage: [sudo] ./gpiod.sh [-d <GPIOD_PATH>] [-v <TEST_PROG_VERSION>]
+				  [-u <TEST_GIT_URL>] [-p <TEST_DIR>] [-s <true|false>]
+
+
+	<GPIOD_PATH>:
+	The pre-installed gpiod path GPIOD_PATH on the rootfs.
+
+	<TEST_PROG_VERSION>:
+	If this parameter is set, then the ${TEST_PROGRAM} is cloned. In
+	particular, the version of the suite is set to the commit
+	pointed to by the parameter. A simple choice for the value of
+	the parameter is, e.g., HEAD. If, instead, the parameter is
+	not set, then the suite present in TEST_DIR is used.
+
+	<TEST_GIT_URL>:
+	If this parameter is set, then the ${TEST_PROGRAM} is cloned
+	from the URL in TEST_GIT_URL. Otherwise it is cloned from the
+	standard repository for the suite. Note that cloning is done
+	only if TEST_PROG_VERSION is not empty
+
+	<TEST_DIR>:
+	If this parameter is set, then the ${TEST_PROGRAM} suite is cloned to or
+		looked for in TEST_DIR. Otherwise it is cloned to $(pwd)/${TEST_PROGRAM}
+
+	<SKIP_INSTALL>:
+	If you already have it installed into the rootfs.
+	default: false"
+}
+
+while getopts "d:hk:p:u:s:v:" opt; do
+	case $opt in
+		d)
+			if [[ "$OPTARG" != '' ]]; then
+				GPIOD_PATH="$OPTARG"
+			fi
+			;;
+		u)
+			if [[ "$OPTARG" != '' ]]; then
+				TEST_GIT_URL="$OPTARG"
+			fi
+			;;
+		p)
+			if [[ "$OPTARG" != '' ]]; then
+				TEST_DIR="$OPTARG"
+			fi
+			;;
+		s)
+			SKIP_INSTALL="${OPTARG}"
+			;;
+		v)
+			TEST_PROG_VERSION="$OPTARG"
+			;;
+		h)
+			usage
+			exit 0
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
+
+install() {
+	dist=
+	dist_name
+	case "${dist}" in
+		debian|ubuntu)
+			pkgs="git libtool automake build-essential autoconf-archive pkg-config"
+			install_deps "${pkgs}" "${SKIP_INSTALL}"
+			;;
+		fedora|centos)
+			pkgs=""
+			install_deps "${pkgs}" "${SKIP_INSTALL}"
+			;;
+		# When build do not have package manager
+		# Assume dependencies pre-installed
+		*)
+			echo "Unsupported distro: ${dist}! Package installation skipped!"
+			;;
+	esac
+}
+
+build_install_tests() {
+	pushd "${TEST_DIR}" || exit 1
+	./autoconfig.sh --enable-tools=yes --prefix=
+	make
+	make install
+	popd || exit 1
+}
+
+if [ "${SKIP_INSTALL}" = "true" ] || [ "${SKIP_INSTALL}" = "True" ]; then
+	info_msg "Skip installing package dependency for ${TEST_PROG_VERSION}"
+else
+	install
+fi
+
+get_test_program "${TEST_GIT_URL}" "${TEST_DIR}" "${TEST_PROG_VERSION}" "${TEST_PROGRAM}"
+
+if [ ! -d "${GPIOD_PATH}/bin" ]; then
+	build_install_tests
+fi
+create_out_dir "${OUTPUT}"
+run_test
+
+export PATH="${GPIOD_PATH}/bin:$PATH"
 gpiod-test 2>&1| tee tmp.txt
 sed 's/\[[0-9;]*m//g'  tmp.txt \
 	| grep '\[TEST\]' \
