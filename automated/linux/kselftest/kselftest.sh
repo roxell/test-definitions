59a975495797a15c2a269ad4f66304c43e697e30
Anders Roxell <anders.roxell@linaro.org>
2022-11-09 12:53:57 +0100

    kselftest: add sharding
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/kselftest/kselftest.sh
+++ automated/linux/kselftest/kselftest.sh
@@ -30,7 +30,9 @@ else
 fi
 
 usage() {
-    echo "Usage: $0 [-c bpf cpufreq net timers]
+    echo "Usage: $0 [-a sharding bucket to run ]
+                    [-b number of shard buckets to create ]
+                    [-c bpf cpufreq net timers]
                     [-T cpu-hotplug:cpu-on-off-test.sh]
                     [-t kselftest_aarch64.tar.gz | kselftest_armhf.tar.gz]
                     [-s True|False]
@@ -44,8 +46,10 @@ usage() {
     exit 1
 }
 
-while getopts "c:T:t:s:u:p:L:S:b:g:e:h" opt; do
+while getopts "a:b:c:T:t:s:u:p:L:S:b:g:e:h" opt; do
     case "${opt}" in
+        a) SHARD_INDEX="${OPTARG}" ;;
+        b) SHARD_NUMBER="${OPTARG}" ;;
         c) TST_CMDFILES="${OPTARG}" ;;
         T) TST_CASENAME="${OPTARG}" ;;
         t) TESTPROG="${OPTARG}" ;;
@@ -213,8 +217,16 @@ rm -f "${skips}"
 if [ -n "${TST_CASENAME}" ]; then
     ./run_kselftest.sh -t "${TST_CASENAME}" 2>&1 | tee -a "${LOGFILE}"
 elif [ -n "${TST_CMDFILES}" ]; then
+    cp kselftest-list.txt kselftest-list.txt.skips
     # shellcheck disable=SC2086
     for test in ${TST_CMDFILES}; do
+        cp kselftest-list.txt.skips kselftest-list.txt
+        cat kselftest-list.txt | grep "^${test}:" | tee kselftest-list.txt
+        split --verbose --numeric-suffixes=1 -n l/"${SHARD_INDEX}"/"${SHARD_NUMBER}" kselftest-list.txt > shardfile
+        echo "============== Tests to run ==============="
+        cat shardfile
+        echo "===========End Tests to run ==============="
+        cp shardfile kselftest-list.txt
         ./run_kselftest.sh -c ${test} 2>&1 | tee -a "${LOGFILE}"
     done
 else
