609a966409ad4e7d9d228a77a2f6f056e813ba28
Konstantin Belov <konstantin.belov@linaro.org>
2023-10-20 18:29:52 +0200

    automated: linux: mmtests: reworked MMTESTS_TYPE_NAME
    Variable removed from launch command as not required for MMTests.
    MMTESTS_TYPE_NAME renamed to TEST_NAME as it's not related to MMTests.
    In mmtests.sh option MMTESTS_TYPE_NAME removed as deprecated.
    
    Signed-off-by: Konstantin Belov <konstantin.belov@linaro.org>

--- automated/linux/mmtests/mmtests.sh
+++ automated/linux/mmtests/mmtests.sh
@@ -12,7 +12,6 @@ TEST_GIT_URL=https://github.com/gormanm/mmtests
 TEST_DIR=${TEST_DIR:-"$(pwd)/${TEST_PROGRAM}"}
 SKIP_INSTALL=${SKIP_INSTALL:-"false"}
 MMTESTS_MAX_RETRIES=${MMTESTS_MAX_RETRIES:-"3"}
-MMTESTS_TYPE_NAME=
 MMTESTS_CONFIG_FILE=
 MMTEST_ITERATIONS=${MMTEST_ITERATIONS:-"10"}
 MMTEST_EXTR="./bin/extract-mmtests.pl"
@@ -24,8 +23,8 @@ declare -A env_variable_mappings=( ["dbench4"]="DBENCH" )
 usage() {
   echo "\
   Usage: $0 [-s] [-v <TEST_PROG_VERSION>] [-u <TEST_GIT_URL>] [-p <TEST_DIR>]
-          [-c <MMTESTS_CONFIG_FILE>] [-t <MMTESTS_TYPE_NAME>]
-          [-r <MMTESTS_MAX_RETRIES>] [-i <MMTEST_ITERATIONS>]
+          [-c <MMTESTS_CONFIG_FILE>] [-r <MMTESTS_MAX_RETRIES>]
+          [-i <MMTEST_ITERATIONS>]
 
   -v <TEST_PROG_VERSION>
     If this parameter is set, then the ${TEST_PROGRAM} suite is cloned. In
@@ -52,9 +51,6 @@ usage() {
     MMMTests configuration file that describes how the benchmarks should be
     configured and executed.
 
-  -t <MMTESTS_TYPE_NAME>
-    MMTests test type, e.g. sysbenchcpu, iozone, sqlite, etc.
-
   -r <MMTESTS_MAX_RETRIES>
     Maximum number of retries for the single benchmark's source file download.
 
@@ -64,7 +60,7 @@ usage() {
   exit 1
 }
 
-while getopts "c:p:r:st:u:v:i:" opt; do
+while getopts "c:p:r:su:v:i:" opt; do
   case "${opt}" in
     c)
       MMTESTS_CONFIG_FILE="${OPTARG}"
@@ -79,9 +75,6 @@ while getopts "c:p:r:st:u:v:i:" opt; do
       ;;
     s)
       SKIP_INSTALL=true
-      ;;
-    t)
-      MMTESTS_TYPE_NAME="${OPTARG}"
       ;;
     u)
       if [[ "$OPTARG" != '' ]]; then
@@ -144,7 +137,7 @@ prepare_system() {
 }
 
 run_test() {
-  info_msg "Running ${MMTESTS_TYPE_NAME} test..."
+  info_msg "Running ${MMTESTS_CONFIG_FILE} test..."
   # It's required to export MMTEST_ITERATIONS as it will be used by
   # run-mmtests.sh from the MMTests package.
   export MMTEST_ITERATIONS=${MMTEST_ITERATIONS}
@@ -253,7 +246,7 @@ collect_results() {
 ! check_root && error_msg "Please run this script as root."
 
 if [ "${SKIP_INSTALL}" = "true" ]; then
-  info_msg "${MMTESTS_TYPE_NAME} installation skipped"
+  info_msg "Installation skipped"
 else
   # Install system-wide dependencies.
   install_system_deps
