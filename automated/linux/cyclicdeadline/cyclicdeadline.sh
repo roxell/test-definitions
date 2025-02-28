8fc94bab0203f32781dcd68b110db1e54bd51dbe
Anders Roxell <anders.roxell@linaro.org>
2025-02-28 23:17:36 +0100

    automated: linux: add cyclicdeadline
    This will make it possible to run multiple iterations to gather multiple
    min/max/avg values.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/cyclicdeadline/cyclicdeadline.sh
+++ automated/linux/cyclicdeadline/cyclicdeadline.sh
@@ -7,7 +7,7 @@
 . ../../lib/sh-test-lib
 
 OUTPUT="$(pwd)/output"
-LOGFILE="${OUTPUT}/cyclicdeadline.json"
+LOGFILE="${OUTPUT}/cyclicdeadline"
 RESULT_FILE="${OUTPUT}/result.txt"
 
 INTERVAL="1000"
@@ -15,19 +15,21 @@ STEP="500"
 THREADS="1"
 DURATION="1m"
 BACKGROUND_CMD=""
+ITERATIONS=1
 
 usage() {
-    echo "Usage: $0 [-i interval] [-s step] [-t threads] [-D duration ] [-w background_cmd]" 1>&2
+    echo "Usage: $0 [-i interval] [-s step] [-t threads] [-D duration ] [-w background_cmd] [-I iterations]" 1>&2
     exit 1
 }
 
-while getopts ":i:s:t:D:w:" opt; do
+while getopts ":i:s:t:D:w:I:" opt; do
     case "${opt}" in
         i) INTERVAL="${OPTARG}" ;;
         s) STEP="${OPTARG}" ;;
         t) THREADS="${OPTARG}" ;;
         D) DURATION="${OPTARG}" ;;
 	w) BACKGROUND_CMD="${OPTARG}" ;;
+        I) ITERATIONS="${OPTARG}" ;;
         *) usage ;;
     esac
 done
@@ -48,11 +50,24 @@ fi
 
 background_process_start bgcmd --cmd "${BACKGROUND_CMD}"
 
-"${binary}" -q -i "${INTERVAL}" -s "${STEP}" -t "${THREADS}" \
-	-D "${DURATION}" --json="${LOGFILE}"
+counter=0
+while [ ${counter} -lt ${ITERATIONS} ]; do
+    "${binary}" -q -i "${INTERVAL}" -s "${STEP}" -t "${THREADS}" \
+        -D "${DURATION}" --json="${LOGFILE}-${counter}.json"
+    counter=$((counter + 1))
+done
 
 background_process_stop bgcmd
 
 # Parse test log.
-../../lib/parse_rt_tests_results.py cyclicdeadline "${LOGFILE}" \
-    | tee -a "${RESULT_FILE}"
+counter=0
+while [ ${counter} -lt ${ITERATIONS} ]; do
+../../lib/parse_rt_tests_results.py cyclicdeadline "${LOGFILE}-${counter}.json" \
+        | tee /tmp/output
+
+    if [ ${ITERATIONS} -ne 1 ]; then
+        sed -i "s|^|iteration-${counter}-|g" /tmp/output
+    fi
+    cat /tmp/output | tee -a "${RESULT_FILE}"
+    counter=$((counter + 1))
+done
