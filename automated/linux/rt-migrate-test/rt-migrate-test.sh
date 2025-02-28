4d192e43db47632f8b009e909ecb57661bd5cb5b
Anders Roxell <anders.roxell@linaro.org>
2025-02-28 22:39:34 +0100

    automated: linux: add rt-migrate-test
    This will make it possible to run multiple iterations to gather multiple
    min/max/avg values.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- automated/linux/rt-migrate-test/rt-migrate-test.sh
+++ automated/linux/rt-migrate-test/rt-migrate-test.sh
@@ -5,22 +5,24 @@
 . ../../lib/sh-test-lib
 
 OUTPUT="$(pwd)/output"
-LOGFILE="${OUTPUT}/rt-migrate-test.json"
+LOGFILE="${OUTPUT}/rt-migrate-test"
 RESULT_FILE="${OUTPUT}/result.txt"
 PRIORITY="51"
 DURATION="1m"
 BACKGROUND_CMD=""
+ITERATIONS=1
 
 usage() {
-    echo "Usage: $0 [-p priority] [-D duration] [-w background_cmd]" 1>&2
+    echo "Usage: $0 [-p priority] [-D duration] [-w background_cmd] [-i iterations]" 1>&2
     exit 1
 }
 
-while getopts ":l:p:D:w:" opt; do
+while getopts ":l:p:D:w:i:" opt; do
     case "${opt}" in
 	p) PRIORITY="${OPTARG}" ;;
 	D) DURATION="${OPTARG}" ;;
 	w) BACKGROUND_CMD="${OPTARG}" ;;
+        i) ITERATIONS="${OPTARG}" ;;
         *) usage ;;
     esac
 done
@@ -37,10 +39,24 @@ fi
 
 background_process_start bgcmd --cmd "${BACKGROUND_CMD}"
 
-"${binary}" -q -p "${PRIORITY}" -D "${DURATION}" -c --json="${LOGFILE}"
+counter=0
+while [ ${counter} -lt ${ITERATIONS} ]; do
+    "${binary}" -q -p "${PRIORITY}" -D "${DURATION}" -c --json="${LOGFILE}-${counter}.json"
+    counter=$((counter + 1))
+done
+
 
 background_process_stop bgcmd
 
 # Parse test log.
-../../lib/parse_rt_tests_results.py rt-migrate-test "${LOGFILE}" \
-    | tee -a "${RESULT_FILE}"
+counter=0
+while [ ${counter} -lt ${ITERATIONS} ]; do
+../../lib/parse_rt_tests_results.py rt-migrate-test "${LOGFILE}-${counter}.json" \
+        | tee /tmp/output
+
+    if [ ${ITERATIONS} -ne 1 ]; then
+        sed -i "s|^|iteration-${counter}-|g" /tmp/output
+    fi
+    cat /tmp/output | tee -a "${RESULT_FILE}"
+    counter=$((counter + 1))
+done
